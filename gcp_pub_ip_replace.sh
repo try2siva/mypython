49f0ff8f2402cc4dd06a00aa07e27e6bd2012c61
First Last <sivakumarbecse@gmail.com>
2019-08-05 06:20:50 +0000

    New file adding

---
+++ gcp_pub_ip_replace.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ip1=$(grep -i "test.local" /tmp/hosts | awk '{print $1}')
+ip2=`curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip > /tmp/current_dynamic_ip`
+pub_ip=`cat /tmp/current_dynamic_ip`
+if [ "$ip1" != "$pub_ip" ]; then
+	echo $(`sed -i s/$ip1/$pub_ip/ /tmp/hosts` echo "New Public GCP instance IP replaced")
+else
+	echo "Existing Public GCP instance IP and Current IP matching"
+fi
