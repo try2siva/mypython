e279414c4c7eec462d74538a1dc49c23082781df
try2siva <sivakumarbecse@gmail.com>
2019-08-05 07:42:27 +0000

    updated file aws_pub_ip_replace

--- aws_pub_ip_replace.sh
+++ aws_pub_ip_replace.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ip1=$(grep -i "test.local" /tmp/hosts | awk '{print $1}')
+ip1=$(grep -i "test.local" /etc/hosts | awk '{print $1}')
 ip2=`curl http://169.254.169.254/latest/meta-data/public-ipv4 > /tmp/current_dynamic_ip`
 pub_ip=`cat /tmp/current_dynamic_ip`
 if [ "$ip1" != "$pub_ip" ]; then
-        echo $(`sed -i s/$ip1/$pub_ip/ /tmp/hosts` echo "New Public GCP instance IP replaced")
+        echo $(`sed -i s/$ip1/$pub_ip/ /etc/hosts` echo "New Public GCP instance IP replaced")
 else
         echo "Existing Public GCP instance IP and Current IP matching"
 fi
